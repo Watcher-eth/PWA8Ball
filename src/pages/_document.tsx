7cd8a8410823a81d69e8476733c5a81c3905077a
caesar <caesar0x@users.noreply.github.com>
2024-06-30 19:02:04 -0400

    check if hardcoding meta renders

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,14 @@
+import { OG_API_SPLASH_URL } from "@/utils/urls";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:image" content={OG_API_SPLASH_URL} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Head>
 
       <body>
         <Main />
