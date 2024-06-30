b9afe13bfb70370c174b615f67df9e216dff43a8
caesar <caesar0x@users.noreply.github.com>
2024-06-30 19:09:26 -0400

    move constant head data into _document.tsx so next correctly picks it up and includes it

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -45,34 +45,6 @@ export default function App({ Component, pageProps }: AppProps) {
         }}
       >
         <AuthChecker>
-          <Head>
-            <meta name="application-name" content={APP_NAME} />
-            <meta name="apple-mobile-web-app-capable" content="yes" />
-            <meta name="apple-mobile-web-app-title" content={APP_NAME} />
-            <meta name="description" content={APP_DESCRIPTION} />
-            <meta name="format-detection" content="telephone=no" />
-            <meta name="mobile-web-app-capable" content="yes" />
-            <meta name="apple-mobile-web-app-capable" content="yes" />
-            <meta
-              name="apple-mobile-web-app-status-bar-style"
-              content="black-translucent"
-            />
-            <meta name="theme-color" content="#FFFFFF" />
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, viewport-fit=cover"
-            />
-            <meta property="og:image" content={OG_API_SPLASH_URL} />
-            <meta property="og:image:width" content="1200" />
-            <meta property="og:image:height" content="630" />
-            <link
-              rel="apple-touch-icon"
-              sizes="180x180"
-              href="/icons/AppIcon512.png"
-            />
-            <link rel="manifest" href="/manifest.json" />
-            <link rel="shortcut icon" href="/favicon.ico" />
-          </Head>
           <Layout>
             <WrappedComponent {...pageProps} />
           </Layout>
