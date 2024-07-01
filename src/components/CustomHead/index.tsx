c909d9185af8c70f0abd32bc00f722589300f197
caesar <caesar0x@users.noreply.github.com>
2024-06-30 23:15:36 -0400

    fuck it we do it live

--- src/components/CustomHead/index.tsx
+++ src/components/CustomHead/index.tsx
@@ -1,16 +1,21 @@
 // @ts-nocheck
-import { OG_API_SPLASH_URL, getApiOgTopicUrl, getTopicUrl } from "@/utils/urls";
+import { OG_API_SPLASH_URL, getApiOgRouteUrl, getApiOgTopicUrl, getMarketPreviewUrl, getMarketUrl, getTopicUrl } from "@/utils/urls";
 import { NextSeo } from "next-seo";
-import Head from "next/head"
 
-export function CustomHead({ topicData, router, ...rest }) {
-  console.log({topicData})
+export function CustomHead({ topicData, userId, router, ...rest }) {
   let headSeo
   if (topicData) {
     headSeo = <TopicSeo {...topicData} />
+  } else if (userId) {
+    headSeo = <ProfileSeo userId={userId} />
+  } else if (router?.query?.id) {
+    // NOTE: This if statement needs to be more specific given its rn
+    //       likely to catch too much
+    headSeo = <MarketSeo id={router?.query?.id} />
   } else {
     headSeo = <SplashSeo />
   }
+
   return (
     <>
       {headSeo}
@@ -55,6 +60,56 @@ function TopicSeo({ id, name, description, image, title, icon, topic, type, memb
             width: 1200,
             height: 630,
             alt: "Topic Cover Image",
+          },
+        ],
+      }}
+      twitter={DEFAULT_TWITTER_INFO}
+    />
+  );
+}
+
+function MarketSeo({ id }) {
+  return (
+    <NextSeo
+      openGraph={{
+        type: "website",
+        //     title: market.title,
+        //   description: market.question,
+        url: getMarketUrl(id),
+        images: [
+          {
+            url: getMarketPreviewUrl(id), // Use the API endpoint to generate the image
+            width: 1200,
+            height: 630,
+          },
+        ],
+      }}
+      twitter={{
+        ...DEFAULT_TWITTER_INFO,
+        card: "summary_large_image",
+        //   title: market.title,
+        //   description: market.question,
+        images: [getMarketPreviewUrl(id)],
+      }}
+    />
+  );
+}
+
+
+function ProfileSeo({ userId }) {
+  const ogUrl = getApiOgRouteUrl(userId);
+
+  return (
+    <NextSeo
+      openGraph={{
+        type: "website",
+        url: getProfileUrl(userId),
+        images: [
+          {
+            url: ogUrl,
+            width: 1200,
+            height: 630,
+            alt: "Profile Cover Image",
           },
         ],
       }}
