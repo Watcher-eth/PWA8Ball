bdca6dbebace5f07c712f7e651b40cdab57c2c85
caesar <caesar0x@users.noreply.github.com>
2024-07-05 15:29:19 -0400

    fix

--- src/lib/onchain/mutations/BoostMarket.tsx
+++ src/lib/onchain/mutations/BoostMarket.tsx
@@ -9,29 +9,35 @@ import {
   getEightBallContract,
 } from "../contracts/Eightball";
 import { rpcClient } from "../Viem";
-import { WalletClient } from "viem";
+import { WalletClient, Address } from "viem";
 import { addLiquidityBoost } from "@/lib/supabase/mutations/addBoost";
 
-interface boostMarket {
+
+
+async function boost({
+  amount,
+  userId,
+  marketId,
+  client,
+  address,
+}: {
   amount: number;
   userId: string;
   marketId: number;
   client: WalletClient;
-  address: `0x${string}`;
-}
-
-async function boost(props: boostMarket) {
-  console.log("Amount", props.amount);
-  if (!props.amount || !props.marketId) {
+  address: Address;
+}) {
+  console.log("Amount", amount);
+  if (!amount || !marketId) {
     throw new Error("All fields must be provided");
   }
-  console.log("Props", props);
+  // console.log("Props", props);
   try {
     // Convert the _Amount to USDC's correct unit (typically 6 decimals)
     const adjustedAmount = ethers.utils
-      .parseUnits(props.amount.toString(), 6)
+      .parseUnits(amount.toString(), 6)
       .toBigInt();
-    const account = props.address;
+    const account = address;
     const allowance = await rpcClient.readContract({
       address: "0x036CbD53842c5426634e7929541eC2318f3dCF7e",
       abi: UsdcABI,
@@ -39,7 +45,7 @@ async function boost(props: boostMarket) {
       functionName: "allowance",
     });
     console.log("current allowance", allowance);
-    if (allowance < BigInt(props.amount)) {
+    if (allowance < BigInt(amount)) {
       // Approve the USDC transfer
       const { request: usdcRequest } = await rpcClient.simulateContract({
         account,
@@ -49,7 +55,7 @@ async function boost(props: boostMarket) {
         functionName: "approve",
       });
 
-      const usdcApproval = await props.client.writeContract(usdcRequest);
+      const usdcApproval = await client.writeContract(usdcRequest);
       console.log("Approved USDC", usdcApproval, account);
     }
 
@@ -58,16 +64,16 @@ async function boost(props: boostMarket) {
       account,
       address: EightBallAddress,
       abi: EightballV1ABI,
-      args: [BigInt(props.amount), BigInt(props.marketId)],
+      args: [BigInt(amount), BigInt(marketId)],
       functionName: "boostMarket",
     });
 
-    const result = await props.client.writeContract(request);
+    const result = await client.writeContract(request);
 
     const send = await addLiquidityBoost({
-      user_id: props.userId,
-      market_id: props.marketId,
-      amount_added: props.amount,
+      user_id: userId,
+      market_id: marketId,
+      amount_added: amount,
     });
   } catch (error) {
     console.error("Error during market boost", error);
