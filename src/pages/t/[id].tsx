095b5063776a35c8d2f3f7b20d0ccd195d7fdf04
caesar <caesar0x@users.noreply.github.com>
2024-07-07 14:42:52 -0400

    we need to standardize the url routing schema, links keep breaking

--- src/pages/t/[id].tsx
+++ src/pages/t/[id].tsx
@@ -1,10 +1,9 @@
 // @ts-nocheck
 
-import { FeaturedBet } from "@/components/Topics";
-import { GetServerSideProps, Metadata, ResolvingMetadata } from "next";
-import { useRouter } from "next/router";
+
+import { GetServerSideProps } from "next";
 import { supabase } from "@/lib/supabase/supabaseClient";
-import { getApiOgTopicUrl, getTopicUrl } from "@/utils/urls";
+import { FeaturedBet } from "@/components/Topics";
 
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
@@ -35,29 +34,36 @@ export default function TopicPage({
 }: {
   topicData: any;
 }) {
-  const router = useRouter();
   console.log({ topicData });
-  if (!topicData) {
-    console.log("loading");
+
+  if (topicData) {
+    const {
+      id,
+      name,
+      description,
+      image,
+      title,
+      icon,
+      topic,
+      type,
+      members
+    } = topicData;
+
+    return (
+      <div>
+        <FeaturedBet
+          id={id}
+          name={title}
+          description={description}
+          image={image}
+          icon={icon}
+          topic={topic}
+          type={type}
+          members={parseInt(members, 10)}
+        />
+      </div>
+    );
+  } else {
     return <p>Loading...</p>;
   }
-
-  const { id, name, description, image, title, icon, topic, type, members } =
-    topicData;
-  const ogUrl = getApiOgTopicUrl(id);
-
-  return (
-    <div>
-      <FeaturedBet
-        id={id}
-        name={title}
-        description={description}
-        image={image}
-        icon={icon}
-        topic={topic}
-        type={type}
-        members={parseInt(members, 10)}
-      />
-    </div>
-  );
 };
