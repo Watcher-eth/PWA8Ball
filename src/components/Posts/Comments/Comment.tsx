76774aac057c53b72f7d43c67e79062a14e07d9e
caesar <caesar0x@users.noreply.github.com>
2024-07-09 15:21:42 -0400

    fix redundant

--- src/components/Posts/Comments/Comment.tsx
+++ src/components/Posts/Comments/Comment.tsx
@@ -1,7 +1,6 @@
 // @ts-nocheck
 
 import React, { useState } from "react";
-import { useRouter } from "next/router";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { Heart, ThumbsDown } from "lucide-react";
@@ -21,14 +20,24 @@ interface CommentProps extends BetComment {
   user2: any; // Define the proper type for user2 if possible
 }
 
-export const Comment: React.FC<CommentProps> = (props) => {
+export function Comment({
+  id,
+  user,
+  user2,
+  created_at,
+  content,
+  name,
+  hasPosition,
+  date,
+  setReply,
+  handleComment,
+}: CommentProps) {
   const { mutate: deleteComment } = useDeleteComment();
   const [temporaryLike, setTemporaryLike] = useState(false);
   const [temporaryDislike, setTemporaryDislike] = useState(false);
-  const router = useRouter();
 
   const handleDelete = () => {
-    deleteComment(props.id, {
+    deleteComment(id, {
       onSuccess: (data) => {
         console.log("Deletion successful, deleted data:", data);
       },
@@ -56,59 +65,55 @@ export const Comment: React.FC<CommentProps> = (props) => {
       transition={{ duration: 0.5 }}
       className="flex flex-col w-full"
     >
-      <div
-        className="flex flex-row w-full items-center justify-between mt-5"
-      >
+      <div className="flex flex-row w-full items-center justify-between mt-5">
         <Link
           className="cursor-pointer"
-          href={getProfilePath(props.user2?.external_auth_provider_user_id)}
+          href={getProfilePath(user2?.external_auth_provider_user_id)}
         >
           <div className="flex flex-row items-center">
             <img
-              src={props?.user?.pfp}
+              src={user?.pfp}
               alt="profile"
               className="size-12 rounded-[50%] overflow-hidden object-cover mr-1.5"
             />
             <div className="flex flex-col">
               <div className="flex flex-row items-center">
                 <p className="text-[16.5px] font-[Aeonik-Bold] text-white">
-                  {props?.user?.name}
+                  {user?.name}
                 </p>
-                {props?.hasPosition === true && (
+                {hasPosition && (
                   <p
                     className={`
                       text-[12px] font-[Aeonik-Bold] text-white
                       px-1.5 py-px rounded-[10px] overflow-hidden ml-1.5
                       ${
-                        parseOption(props?.user2?.option) === "No"
+                        parseOption(user2?.option) === "No"
                           ? "bg-[#FF0050]"
                           : "bg-[#0050FF]"
                       }
                     `}
                   >
-                    ${(props?.user2?.amount / 10 ** 6).toFixed(2)}{" "}
-                    {parseOption(props?.user2?.option)}
+                    ${(user2?.amount / 10 ** 6).toFixed(2)}{" "}
+                    {parseOption(user2?.option)}
                   </p>
                 )}
               </div>
 
               <p className="text-[14.5px] text-lightgray mt-2">
                 Replied{" "}
-                {timeAgo(props.created_at ? props.created_at : props.date)}
+                {timeAgo(created_at ?? date)}
               </p>
             </div>
           </div>
         </Link>
       </div>
 
-      <p className="my-2 text-[17px] text-white">
-        {props.content}
-      </p>
+      <p className="my-2 text-[17px] text-white">{content}</p>
       <div className="flex flex-row items-center justify-between mb-2">
         <button
           onClick={() => {
-            props.setReply(props.user ? props.user.name : props.name);
-            props.handleComment();
+            setReply(user?.name ?? name);
+            handleComment();
           }}
           className={`
             text-[15px] font-[Aeonik-Bold] text-[#DDDDDD]
@@ -117,9 +122,7 @@ export const Comment: React.FC<CommentProps> = (props) => {
         >
           Reply
         </button>
-        <div
-          className="flex flex-row items-center mb-1.5"
-        >
+        <div className="flex flex-row items-center mb-1.5">
           <button
             onClick={handleLikePress}
             className={`
@@ -140,13 +143,6 @@ export const Comment: React.FC<CommentProps> = (props) => {
               bg-none border-none cursor-pointer
               flex items-center
             `}
-            style={{
-              background: "none",
-              border: "none",
-              cursor: "pointer",
-              display: "flex",
-              alignItems: "center",
-            }}
           >
             <ThumbsDown
               fill={temporaryDislike ? "#FF6700" : "transparent"}
@@ -157,9 +153,7 @@ export const Comment: React.FC<CommentProps> = (props) => {
           </button>
         </div>
       </div>
-      <div
-        className="w-[110%] self-center h-[0.35px] bg-[#303030] mt-1.5 mb-px z-20"
-      />
+      <div className="w-[110%] self-center h-[0.35px] bg-[#303030] mt-1.5 mb-px z-20" />
     </motion.div>
   );
 };
