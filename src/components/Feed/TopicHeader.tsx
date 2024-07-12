39179081edf98eb7e8b084778cd41ae862ce37f6
caesar <caesar0x@users.noreply.github.com>
2024-07-12 17:00:28 -0400

    fix

--- src/components/Feed/TopicHeader.tsx
+++ src/components/Feed/TopicHeader.tsx
@@ -18,13 +18,6 @@ const testTopics = [
 ];
 
 export const TopicHeader = ({ setSelectedTopic, selectedTopic, isDesktop }) => {
-  function handleTopicPress(item) {
-    if (selectedTopic === item) {
-      setSelectedTopic("🔥 Trending");
-    } else {
-      setSelectedTopic(item);
-    }
-  };
 
   return (
     <div
@@ -36,49 +29,60 @@ export const TopicHeader = ({ setSelectedTopic, selectedTopic, isDesktop }) => {
       <Marquee speed={40} gradient={false}>
         <div className="flex flex-row">
           {testTopics.slice(0, testTopics.length / 2).map((item, index) => (
-            <motion.div
+            <TopicButton
               key={index}
-              initial={{ opacity: 0, x: 50 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.2 * (index + 1) }}
-            >
-              <button
-                onClick={() => handleTopicPress(item)}
-                className={`px-4 py-2 text-base font-semibold rounded-full mr-4 ${
-                  selectedTopic === item
-                    ? "bg-white text-[#1B1B1E]"
-                    : "bg-[#1B1B1E] text-white"
-                }`}
-              >
-                {item}
-              </button>
-            </motion.div>
+              item={item}
+              index={index}
+              selectedTopic={selectedTopic}
+              setSelectedTopic={setSelectedTopic}
+            />
           ))}
         </div>
       </Marquee>
       <Marquee speed={20} gradient={false} className="mt-3">
         <div className="flex flex-row">
           {testTopics.slice(testTopics.length / 2).map((item, index) => (
-            <motion.div
+            <TopicButton
               key={index}
-              initial={{ opacity: 0, x: 50 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.2 * (index + 1) }}
-            >
-              <button
-                onClick={() => handleTopicPress(item)}
-                className={`px-4 py-2 text-base font-semibold rounded-full mr-4 ${
-                  selectedTopic === item
-                    ? "bg-white text-[#1B1B1E]"
-                    : "bg-[#1B1B1E] text-white"
-                }`}
-              >
-                {item}
-              </button>
-            </motion.div>
+              item={item}
+              index={index}
+              selectedTopic={selectedTopic}
+              setSelectedTopic={setSelectedTopic}
+            />
           ))}
         </div>
       </Marquee>
     </div>
   );
 };
+
+
+function TopicButton({ item, index, selectedTopic, setSelectedTopic }) {
+  function onTopicPress() {
+    if (selectedTopic === item) {
+      setSelectedTopic("🔥 Trending");
+    } else {
+      setSelectedTopic(item);
+    }
+  }
+
+  return (
+    <motion.div
+      key={index}
+      initial={{ opacity: 0, x: 50 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.2 * (index + 1) }}
+    >
+      <button
+        onClick={onTopicPress}
+        className={`px-4 py-2 text-base font-semibold rounded-full mr-4 ${
+          selectedTopic === item
+            ? "bg-white text-[#1B1B1E]"
+            : "bg-[#1B1B1E] text-white"
+        }`}
+      >
+        {item}
+      </button>
+    </motion.div>
+  );
+}
