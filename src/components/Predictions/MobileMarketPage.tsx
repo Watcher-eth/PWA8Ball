46390a5a42fc87ea9500b983361228d7b83d60b3
caesar <caesar0x@users.noreply.github.com>
2024-07-14 15:07:08 -0400

    MobileDrawerContent

--- src/components/Predictions/MobileMarketPage.tsx
+++ src/components/Predictions/MobileMarketPage.tsx
@@ -1,58 +1,236 @@
+// @ts-nocheck
+import "../../styles/fonts.css";
+import { motion } from 'framer-motion';
 
-
-import React, { useEffect, useState } from "react";
-import { motion, LayoutGroup } from 'framer-motion';
-
-import { DesktopNavbar } from "@/components/Common/DesktopNavbar";
 import { PredictModal } from "@/components/Modals/PredictModal";
 import { ShareModal } from "@/components/Modals/ShareModal";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { useModalStore } from "@/lib/stores/ModalStore";
 import { useUserStore } from "@/lib/stores/UserStore";
-import { useGetUsersByMarketId } from "@/supabase/queries/markets/useGetUsersByMarketId";
-import { useGetMarketById } from "@/supabase/queries/useGetMarketById";
 
 import { BettersOverviewModal } from "@/components/Predictions/Betters/OverviewModal";
 import { CommentSection } from "@/components/Posts/Comments/CommentSection";
 import { BetDetails } from "@/components/Predictions/Details";
 import { RelatedMarkets } from "@/components/Predictions/RelatedMarkets";
 import { fillUserImages } from "@/utils/fillUserImages";
-import { DisplayFeedDrawerContent } from "../Feed/FeedCard";
-import { formatMarket } from "../Feed/formatMarketArr";
-import { Drawer, DrawerContent } from '@/components/ui/drawer';
-import { useRouter } from "next/router";
 
-import { useIsMobile } from "@/hooks/useIsMobile";
+import { formatMarket } from "../Feed/formatMarketArr";
+import { Drawer, DrawerClose, DrawerContent } from '@/components/ui/drawer';
+
+import Link from "next/link";
+
+
+import { ArrowLeft, Share } from "lucide-react";
+import { MobileDrawerContainer } from "@/components/ui/MobileDrawerContainer";
 
 export function MobileMarketPage({ market, users }) {
   const { user } = useUserStore();
   const openLoginModal = useModalStore((state) => state.openLoginModal);
   const userImages = fillUserImages(users, 3);
-  const isMobile = useIsMobile();
 
   return (
-    <div suppressHydrationWarning={true}>
-      <LayoutGroup>
-        <motion.div layout className="font-[Aeonik-Bold] sm:hidden">
-          <Drawer open={isMobile}>
-            <DrawerContent className=" flex flex-col fixed max-h-[103%] border-0 rounded-3xl items-center self-center">
-                <DisplayFeedDrawerContent
-                  setIsDrawerOpen={() => {}}
-                  users={users}
-                  market={market}
-                  userImages={userImages}
-                  openLoginModal={openLoginModal}
-                  handleOpen={() => {}}
-                  {...formatMarket(market)}
-                />
-
-            </DrawerContent>
-          </Drawer>
-        </motion.div>
-      </LayoutGroup>
-    </div>
+    <MobileDrawerContainer>
+      <MobileMarketContent
+        setIsDrawerOpen={() => {}}
+        users={users}
+        market={market}
+        userImages={userImages}
+        openLoginModal={openLoginModal}
+        handleOpen={() => {}}
+        {...formatMarket(market)}
+      />
+    </MobileDrawerContainer>
   );
 }
 
 
 
+
+
+function MobileMarketContent({
+  image,
+  title,
+  description,
+  icon,
+  setIsDrawerOpen,
+  users,
+  market,
+  userImages,
+  openLoginModal,
+  id,
+  stake,
+  multiplier,
+  topicId,
+  optionA,
+  optionB,
+  topicBio,
+  handleOpen,
+  topic,
+}) {
+  console.log("this ran");
+  return (
+    <motion.div
+      onClick={() => setIsDrawerOpen(false)}
+      className="bg-[#070707] w-screen overflow-auto flex flex-col"
+    >
+      <div className="relative h-[100vw]">
+        <div className="flex items-center absolute top-3 justify-between px-6 py-2 w-screen z-[2]">
+          <DrawerClose>
+            <Link href="/" prefetch={true}>
+              <ArrowLeft
+                strokeWidth={3.8}
+                size={33}
+                className="p-2 rounded-full backdrop-blur-lg bg-[rgba(17,17,17,0.15)]"
+              />
+            </Link>
+          </DrawerClose>
+          <ShareModal
+            image={image}
+            title={title}
+            question={description}
+            topic={topic}
+            options={[optionB, optionA]}
+          >
+            <Share
+              size={33}
+              strokeWidth={3.3}
+              className="p-2 rounded-full backdrop-blur-xl bg-[rgba(17,17,17,0.15)]"
+            />
+          </ShareModal>
+        </div>
+        <motion.div className="w-screen h-[42vh] relative">
+          <img
+            className="w-screen object-cover h-[42vh] relative"
+            alt="CoverImage"
+            src={image}
+          />
+          <div
+            style={{ zIndex: 2 }}
+            className="h-[38vw] w-screen bg-gradient-to-t from-black via-transparent to-transparent absolute bottom-0"
+          />
+        </motion.div>
+      </div>
+      <div
+        style={{
+          zIndex: 2,
+          fontFamily: "Benzin-Bold",
+          lineHeight: "2.4rem",
+          fontSize: title?.length < 14 ? 35 : title?.length < 21 ? 32 : 26.5,
+        }}
+        className="pr-10 mt-[-3.8rem] self-start text-start mb-[-0.7rem] pl-5 pb-0 p-3 text-white text-bold"
+      >
+        {title}
+      </div>
+
+      <div className="flex justify-between mt-4 items-center w-[88vw] mx-5">
+        <div className="flex flex-col">
+          <div
+            className={`
+                      text-[0.85rem] text-gray-200 text-bold
+                      font-[Aeonik-Bold] z-[2]
+                    `}
+          >
+            At stake
+          </div>
+          <div
+            className={`
+                      text-[1.6rem] mt-[-0.25rem] text-white flex items-center
+                      font-[Aeonik-Bold] z-[2]
+                    `}
+          >
+            <div> ${((stake ?? 0) / 100000).toFixed(2)}</div>
+          </div>
+        </div>
+        <BettersOverviewModal
+          title={title}
+          question={description}
+          image={image}
+          optionA={optionA.name}
+          optionB={optionB.name}
+          odds={75}
+          marketId={id}
+          users={users}
+        >
+          <div
+            onClick={openLoginModal}
+            className="flex space-x-[-1rem] mb-3 items-center z-[2]"
+          >
+            <Avatar>
+              <AvatarImage src={userImages[0]} />
+            </Avatar>
+            <Avatar>
+              <AvatarImage src={userImages[1]} />
+            </Avatar>
+            <Avatar className="border-1 border-white">
+              <AvatarImage src={userImages[2]} />
+            </Avatar>
+          </div>
+        </BettersOverviewModal>
+      </div>
+      <div className="h-2 w-full color-gray-300 bg-[gray] mx-5 rounded-full z-20" />
+
+      <div
+        className={`
+                  text-[1.05rem] self-start
+                  -mb-px mt-2 ml-5
+                  text-start text-[lightgray] max-w-[88vw]
+                   z-[2] font-[Aeonik-Bold] leading-[1.35rem]
+                `}
+      >
+        {description}
+      </div>
+      <div className="flex items-center w-[88vw] mt-[-4] justify-between mx-2 z-[2]">
+        <PredictModal
+          handleOpen={handleOpen}
+          image={image}
+          multiplier={optionB.odds / 100}
+          option={0}
+          text={optionB?.name}
+          question={description}
+          odds={market?.outcomea / 100}
+          marketId={id}
+          options={[optionB.name, optionA.name]}
+        />
+        <PredictModal
+          handleOpen={handleOpen}
+          image={image}
+          multiplier={optionA.odds / 100}
+          option={1}
+          text={optionA?.name}
+          question={description}
+          odds={market?.outcomea / 100}
+          marketId={id}
+          options={[optionB.name, optionA.name]}
+        />
+      </div>
+      <div style={{ zIndex: 2 }}>
+        <BetDetails
+          endDate={"12th September, 2024"}
+          icon={icon}
+          multiplier={multiplier}
+          topicId={topicId}
+          members={30}
+          handleBoost={() => {}}
+          joined={false}
+          question={topicBio}
+          image={image}
+          betId={id}
+          topic={topic}
+          id={id}
+        />
+      </div>
+      <div className="z-[2]">
+        <CommentSection
+          topicId={topicId}
+          users={users}
+          totalComments={market?.total_comments}
+          optimisticComments={[]}
+          marketId={id}
+          setReply={() => {}}
+          handleComment={() => {}}
+        />
+      </div>
+      <RelatedMarkets topicId={topicId} id={id} />
+    </motion.div>
+  );
+}
