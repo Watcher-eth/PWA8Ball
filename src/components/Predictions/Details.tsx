89860a293a535b89610daab787f3d63df95a9c14
caesar <caesar0x@users.noreply.github.com>
2024-07-16 16:28:42 -0400

    fix

--- src/components/Predictions/Details.tsx
+++ src/components/Predictions/Details.tsx
@@ -144,7 +144,7 @@ export const BetDetails = ({
             <div
               className={`
                 flex flex-row w-full items-center justify-between
-                p-2 border border-white/10 hover:border-white/20
+                px-4 py-2 border border-white/10 hover:border-white/20
                 hover:scale-101 active:scale-98 transition-all
                 ${joined ? "bg-white" : "bg-transparent"}
               `}
