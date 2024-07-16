f7ef4933a9d5619cbf6def327f2e536c77e28855
caesar <caesar0x@users.noreply.github.com>
2024-07-16 17:29:07 -0400

    MobileNavBtn

--- src/components/Common/NavBar.tsx
+++ src/components/Common/NavBar.tsx
@@ -8,7 +8,7 @@ import {
   UserCircle,
 } from "lucide-react";
 import Link from "next/link";
-import { motion } from "framer-motion";
+
 import { useUserStore } from "@/lib/stores/UserStore";
 import { useAuthModalStore } from "@/lib/stores/ModalStore";
 import { ACTIVITY_PATH, HOME_PATH, getProfilePath } from "@/utils/urls";
@@ -20,52 +20,52 @@ export function NavBar() {
 
   return (
     <div
-      className="bg-black/20 items-center justify-center p-[0.9rem] rounded-full backdrop-blur-sm"
-      style={{
-        backdropFilter: "saturate(100%) blur(35px)",
-        zIndex: 3,
-        alignSelf: "center",
-        border: "0.5px solid rgba(255, 255, 255, 0.4)",
-      }}
+      className={`
+        bg-black/20 items-center justify-center p-[0.9rem] rounded-full backdrop-blur-sm
+        z-[3] self-center border-[0.5px] border-[rgba(255,255,255,0.4)] filter-[saturate(100%)_blur(35px)]
+      `}
     >
       <div className="w-[65vw] max-w-[17rem] flex px-2 justify-between items-center mx-auto">
         <Link href={HOME_PATH}>
-          <div className="active:scale-94">
-            <Home className="h-6 text-white w-6" />
-          </div>
+          <MobileNavBtn IconComponent={Home} />
         </Link>
         {user?.walletaddress ? (
           <>
             <Link href={ACTIVITY_PATH}>
-              <div className="active:scale-94">
-                <ActivityIcon className="h-6 text-white w-6" />
-              </div>
+              <MobileNavBtn IconComponent={ActivityIcon} />
             </Link>
             <Link href={getProfilePath(user.external_auth_provider_user_id)}>
-              <div className="active:scale-94">
-                {user?.pfp ? (
-                  <img className="size-6 rounded-full" src={user?.pfp} />
-                ) : (
-                  <UserCircle className="h-6 text-white w-6" />
-                )}
-              </div>
+              <MobileNavBtn iconSrc={user?.pfp} IconComponent={UserCircle}/>
             </Link>
           </>
         ) : (
           <>
-            <div className="active:scale-94" onClick={openLoginModal}>
-              <ActivityIcon className="h-6 text-white w-6" />
-            </div>
-            <div className="active:scale-94" onClick={openLoginModal}>
-              {user?.pfp ? (
-                <img className="size-6 rounded-full" src={user?.pfp} />
-              ) : (
-                <UserCircle className="h-6 text-white w-6" />
-              )}
-            </div>
+            <MobileNavBtn IconComponent={ActivityIcon} onClick={openLoginModal} />
+            <MobileNavBtn iconSrc={user?.pfp} IconComponent={UserCircle} onClick={openLoginModal} />
           </>
         )}
       </div>
+    </div>
+  );
+}
+
+
+function MobileNavBtn({
+  IconComponent,
+  iconSrc,
+  onClick=(() => {})
+}: {
+  IconComponent?: React.FC,
+  iconSrc?: string,
+  onClick?: () => void
+}) {
+  return (
+    <div className="active:scale-94 transition-all" onClick={onClick}>
+      {iconSrc ? (
+        <img className="size-6 rounded-full" src={iconSrc} />
+      ) : (
+        <IconComponent className="size-6 text-white" />
+      )}
     </div>
   );
 }
