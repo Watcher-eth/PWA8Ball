4f194fff5fd8f3a10c9c3410aad191bc40796b35
caesar <caesar0x@users.noreply.github.com>
2024-07-17 15:49:21 -0400

    do it live

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 // @ts-nocheck
 import { GetServerSideProps } from "next";
-
-import { SmartAccountProvider } from "@/lib/onchain/SmartAccount";
 import { DesktopHomePage } from "@/components/Feed/DesktopHomePage";
 import { MobileHomePage } from "@/components/Feed/MobileHomePage";
 import { MobiTop } from "@/components/ui/MobiTop";
 import { fetchTrendingMarkets } from "@/supabase/queries/useGetTrendingMarkets";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const trendingMarkets = await fetchTrendingMarkets();
