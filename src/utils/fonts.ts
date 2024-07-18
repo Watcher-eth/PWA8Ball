d536e17267273760f4d185501fcd08c17df19d9a
caesar <caesar0x@users.noreply.github.com>
2024-07-18 12:59:51 -0400

    use effecient font

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,6 +1,7 @@
 // Fetch the font data once at the start and keep it in memory
 // Idea is to load the font once so its ready in memory to be
 // resolved by the time req arrives
+// Note, would use woff2 here as well but vercel is fucking retarded & doesnt support it
 export const aeonikFontDataPromise = fetch(
   new URL("../../public/fonts/AeonikTRIAL-Bold.otf", import.meta.url)
 ).then((res) => res.arrayBuffer());
