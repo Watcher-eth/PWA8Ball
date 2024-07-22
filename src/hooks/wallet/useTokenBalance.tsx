88c87ccbc405212d0c743c95cd5c312a12fd24ba
caesar <caesar0x@users.noreply.github.com>
2024-07-22 14:36:33 -0400

    useTokenBalance hook

---
+++ src/hooks/wallet/useTokenBalance.tsx
@@ -0,0 +1,21 @@
+import { erc20Abi, type Address } from "viem";
+import { useAccount, useReadContract } from "wagmi";
+
+export function useTokenBalance({ tokenAddress, address }: { tokenAddress: Address, address?: Address }) {
+  const { chainId, address: connectedAccountAddress } = useAccount();
+
+  const accountAddress = address ?? connectedAccountAddress as Address
+
+  const { data: balance } = useReadContract({
+    address: tokenAddress,
+    abi: erc20Abi,
+    chainId: chainId,
+    functionName: "balanceOf",
+    args: [accountAddress],
+    query: {
+      refetchInterval: 10 * 1_000,
+    },
+  });
+
+  return balance;
+}
