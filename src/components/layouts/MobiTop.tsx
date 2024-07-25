49a02f5b04973c7d2b0f2c28520854d78beb745d
caesar <caesar0x@users.noreply.github.com>
2024-07-25 16:54:40 -0400

    push

--- src/components/layouts/MobiTop.tsx
+++ src/components/layouts/MobiTop.tsx
@@ -1,11 +1,16 @@
-// @ts-nocheck
 import { DesktopNavBar } from "@/components/layouts/DesktopNavBar";
 /**
  * Need to properly do this, this is more an interim solution to get the mobile and desktop
  * components to work together for prototyping
  */
 
-export function MobiTop({ mobile, desktop }) {
+export function MobiTop({
+  mobile,
+  desktop
+}: {
+  mobile: React.ReactNode
+  desktop: React.ReactNode
+}) {
   return (
     <>
       <div className="hidden sm:block">
