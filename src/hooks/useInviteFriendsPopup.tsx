4c2ea78a130e6833defd0e3b406230105da071bc
caesar <caesar0x@users.noreply.github.com>
2024-07-30 12:12:17 -0400

    move outside of loop

--- src/hooks/useInviteFriendsPopup.tsx
+++ src/hooks/useInviteFriendsPopup.tsx
@@ -5,36 +5,35 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
 
 export function useInviteFriendsPopup() {
-  function showPopup() {
-    const now = new Date().getTime();
-    AsyncStorage.setItem("lastShownDate", now.toString());
-
-    setTimeout(() => {
-      router.navigate({ pathname: "findFriendsModal" });
-    }, 1200); // Delay of 1 minute
-  }
-
-  async function navigateIfDue() {
-    const lastShownDate = await AsyncStorage.getItem("lastShownDate");
-    const currentDate = new Date().getTime(); // Get current time in milliseconds
-
-    if (lastShownDate) {
-      const lastDate = parseInt(lastShownDate); // Convert stored string back to number
-      const timeDifference = currentDate - lastDate; // Now we can subtract numbers
-      const daysDifference = timeDifference / (1000 * 3600 * 24);
-
-      if (daysDifference >= 1) {
-        showPopup();
-      }
-    } else {
-      // First time or no data found
-      showPopup();
-    }
-  }
-
   useEffect(() => {
     navigateIfDue();
     showPopup();
   }, []);
 };
 
+function showPopup() {
+  const now = new Date().getTime();
+  AsyncStorage.setItem("lastShownDate", now.toString());
+
+  setTimeout(() => {
+    router.navigate({ pathname: "findFriendsModal" });
+  }, 1200); // Delay of 1 minute
+}
+
+async function navigateIfDue() {
+  const lastShownDate = await AsyncStorage.getItem("lastShownDate");
+  const currentDate = new Date().getTime(); // Get current time in milliseconds
+
+  if (lastShownDate) {
+    const lastDate = parseInt(lastShownDate); // Convert stored string back to number
+    const timeDifference = currentDate - lastDate; // Now we can subtract numbers
+    const daysDifference = timeDifference / (1000 * 3600 * 24);
+
+    if (daysDifference >= 1) {
+      showPopup();
+    }
+  } else {
+    // First time or no data found
+    showPopup();
+  }
+}
