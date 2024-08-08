a2e3e50157c89e17c93b77e986993a5884edc05c
watcher-eth1 <watchereth10@gmail.com>
2024-08-08 15:59:42 +0200

    Topics pt2

--- src/components/topic/DesktopTopic.tsx
+++ src/components/topic/DesktopTopic.tsx
@@ -2,6 +2,12 @@ import { useGetMembersForTopic } from "@/supabase/mutations/topics/useGetMembers
 import { useGetMarketsForTopic } from "@/supabase/queries/useGetMarketsForTopic";
 import { useRouter } from "next/router";
 import React, { useRef } from "react";
+import { StandardPageWrapper } from "../layouts/StandardPageWrapper";
+import {
+  InverseBleedOverlay,
+  InverseVerticalBleedOverlay,
+  StandardBleedOverlay,
+} from "../layouts/StandardBleedOverlay";
 
 function DesktopTopic({
   name,
@@ -20,36 +26,55 @@ function DesktopTopic({
   const { data: markets } = useGetMarketsForTopic(id);
 
   return (
-    <div className="flex flex-col items-center w-full">
-      <img className="h-20 w-full rounded-md" src={image} />
-      
-      <div className="flex flex-row items-center justify-center">
-        <div className="flex flex-row items-center">
-          <img className="h-10 w-10 rounded-md" src={image} />
-        </div>
-        <div className="flex flex-col"></div>
-      </div>
-      <div className="h-1 w-full bg-[#212121] mt-1 mb-10" />
-      <div className="text-[1.5rem] text-[#FF0050] font-[700]">
-        Top Predictions
-      </div>
-      <div className="flex flex-row">
-        {markets?.map((item, index) => {
-          if (index < 2)
-            return (
-              <DesktopTopicItem
-                question={item?.question}
-                title={item.title}
-                image={item.image}
-                created_at={item.created_at}
-                size="large"
+    <StandardPageWrapper className="h-full flex  flex-col">
+      <StandardBleedOverlay>
+        <InverseVerticalBleedOverlay>
+          <div className="w-full h-80 relative">
+            <img
+              className="w-full transform rotate-180 object-cover h-80 relative -mt-40"
+              alt="CoverImage"
+              src={image}
+            />
+            <div className="h-80 w-full bg-gradient-to-t from-[#080808] via-[#080808]/50 to-transparent backdrop-blur-lg absolute bottom-0" />
+            <InverseBleedOverlay>
+              <img
+                className="size-22 md:size-26 lg:size-32 xl:size-34 ml-20 absolute -bottom-12 object-cover  rounded-[0.5rem] mb-4   z-20"
+                src={image}
               />
-            );
-        })}
-      </div>
-      <div className="flex flex-row">
-        {markets?.map((item, index) => {
-          if (index > 2 && index < 6)
+            </InverseBleedOverlay>
+          </div>
+        </InverseVerticalBleedOverlay>
+      </StandardBleedOverlay>
+      <div className="full h-full overflow-y-auto pt-10 px-20 flex flex-col">
+        <div className="flex flex-row  justify-between">
+          <div className="flex flex-col -space-y-2">
+            <div className="text-[2.8rem] text-white font-[700]">{name}</div>
+            <div className="text-[1.2rem] text-[white]/[0.9] font-[400]">
+              {description}
+            </div>
+          </div>
+          <div className="flex flex-col"></div>
+        </div>
+        <div className="h-[0.12rem] w-full bg-[#212121] mt-3.5 mb-8" />
+        <div className="text-[2.2rem] mb-3 text-[#FF0050] font-[Aeonik-Bold]">
+          Top Predictions
+        </div>
+        <div className="flex flex-row space-x-8 mb-8">
+          {markets?.map((item, index) => {
+            if (index < 2)
+              return (
+                <DesktopTopicItem
+                  question={item?.question}
+                  title={item.title}
+                  image={item.image}
+                  created_at={item.created_at}
+                  size="large"
+                />
+              );
+          })}
+        </div>
+        <div className="flex flex-row space-x-5 mb-20">
+          {markets?.map((item, index) => {
             return (
               <DesktopTopicItem
                 question={item?.question}
@@ -59,32 +84,39 @@ function DesktopTopic({
                 size="small"
               />
             );
-        })}
-      </div>
-      <div className=" py-7 bg-[#181818]">
-        <div className="text-[1.25rem] text-white font-[700]">Community</div>
-        <div>comments</div>
-      </div>
-      <div className="flex flex-col py-7">
-        <div className="text-[1.25rem] text-white font-[700]">
-          Popular Today
-        </div>
-        <div className="flex flex-row">
-          {markets?.map((item, index) => {
-            if (index < 3)
-              return (
-                <DesktopTopicItem
-                  question={item?.question}
-                  title={item.title}
-                  image={item.image}
-                  created_at={item.created_at}
-                  size="medium"
-                />
-              );
           })}
         </div>
+        <StandardBleedOverlay>
+          <InverseVerticalBleedOverlay>
+            <div className="w-full py-7 mt-20 bg-[#151515]">
+              <div className="text-[1.65rem] text-white font-[700]">
+                Community
+              </div>
+              <div>comments</div>
+            </div>
+          </InverseVerticalBleedOverlay>
+        </StandardBleedOverlay>
+        <div className="flex flex-col py-7">
+          <div className="text-[1.65rem] my-3 text-white font-[700]">
+            Popular Today
+          </div>
+          <div className="flex flex-row space-x-5">
+            {markets?.map((item, index) => {
+              if (index < 3)
+                return (
+                  <DesktopTopicItem
+                    question={item?.question}
+                    title={item.title}
+                    image={item.image}
+                    created_at={item.created_at}
+                    size="medium"
+                  />
+                );
+            })}
+          </div>
+        </div>
       </div>
-    </div>
+    </StandardPageWrapper>
   );
 }
 
@@ -107,39 +139,49 @@ function DesktopTopicItem(props: DesktopItemProps) {
       title: "text-[1rem]",
       question: "text-[1.3rem]",
       date: "text-[0.85rem]",
+      lineHeight: "leading-[1.55rem]",
     },
     medium: {
       container: "w-1/3",
       image: "h-[22vh]",
       title: "text-[1rem]",
-      question: "text-[1.3rem]",
+      question: "text-[1.1rem]",
       date: "text-[0.85rem]",
+      lineHeight: "leading-[1.35rem]",
     },
     small: {
       container: "w-1/4",
       image: "h-[15vh]",
       title: "text-[1rem]",
-      question: "text-[1.3rem]",
+      question: "text-[1rem]",
       date: "text-[0.85rem]",
+      lineHeight: "leading-[1.25rem]",
     },
   };
 
   const selectedSize = sizeClasses[props.size];
 
   return (
-    <div className={`${selectedSize.container} flex-col rounded-md`}>
-      <img className={`w-full ${selectedSize.image}`} src={props.image} />
+    <div
+      className={`${selectedSize.container} flex-col border-[0.1rem] border-[#151515] rounded-lg`}
+    >
+      <img
+        className={`w-full ${selectedSize.image} object-cover rounded-t-lg `}
+        src={props.image}
+      />
       <div>slider</div>
-      <div className="flex flex-col pt-2.5 pb-1.5 bg-[#181818]">
-        <div className={`text-white px-5 ${selectedSize.title}`}>
+      <div className="flex flex-col pt-2.5 rounded-b-lg pb-1.5 bg-[#151515]">
+        <div className={`text-[lightgray] px-5 ${selectedSize.title}`}>
           {props.title}
         </div>
-        <div className={`text-white px-5 ${selectedSize.question}`}>
+        <div
+          className={`text-white font-[600] ${selectedSize.lineHeight} my-1 px-5 ${selectedSize.question}`}
+        >
           {props.question}
         </div>
-        <div className="h-1 w-full bg-[#212121]" />
-        <div className={`text-lightgray px-5 ${selectedSize.date}`}>
-          {props.created_at}
+        <div className="h-[0.1rem] mt-3 mb-1.5 w-full bg-[#313131]" />
+        <div className={`text-[lightgray] py-1 px-5 ${selectedSize.date}`}>
+          1h ago - 323 Predictors
         </div>
       </div>
     </div>
--- src/pages/t/[id].tsx
+++ src/pages/t/[id].tsx
@@ -1,7 +1,8 @@
 // @ts-nocheck
 import { supabase } from "@/supabase/supabaseClient";
 import { Topic } from "@/components/topic";
-
+import { MobiTop } from "@/components/layouts/MobiTop";
+import DesktopTopic from "@/components/topic/DesktopTopic";
 
 export async function getServerSideProps(context) {
   const { id } = context.params as { id: string };
@@ -24,43 +25,46 @@ export async function getServerSideProps(context) {
       topicData,
     },
   };
-};
+}
 
-export default function TopicPage({
-  topicData,
-}: {
-  topicData: any;
-}) {
+export default function TopicPage({ topicData }: { topicData: any }) {
   console.log({ topicData });
 
   if (topicData) {
-    const {
-      id,
-      name,
-      description,
-      image,
-      title,
-      icon,
-      topic,
-      type,
-      members
-    } = topicData;
+    const { id, name, description, image, title, icon, topic, type, members } =
+      topicData;
 
     return (
       <div>
-        <Topic
-          id={id}
-          name={title}
-          description={description}
-          image={image}
-          icon={icon}
-          topic={topic}
-          type={type}
-          members={parseInt(members, 10)}
+        <MobiTop
+          mobile={
+            <Topic
+              id={id}
+              name={title}
+              description={description}
+              image={image}
+              icon={icon}
+              topic={topic}
+              type={type}
+              members={parseInt(members, 10)}
+            />
+          }
+          desktop={
+            <DesktopTopic
+              id={id}
+              name={title}
+              description={description}
+              image={image}
+              icon={icon}
+              topic={topic}
+              type={type}
+              members={parseInt(members, 10)}
+            />
+          }
         />
       </div>
     );
   } else {
     return <p>Loading...</p>;
   }
-};
+}
