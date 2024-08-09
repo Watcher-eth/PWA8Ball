5c0316f890b0610a667c17e754d4bef2301ee91b
watcher-eth1 <watchereth10@gmail.com>
2024-08-09 22:17:31 +0200

    /up pt1

---
+++ src/components/share/UserPosition.tsx/index.tsx
@@ -0,0 +1,134 @@
+// @ts-nocheck
+
+import React, { useState } from "react";
+import { ArrowDown, X, Share as ShareIcon } from "lucide-react";
+import { motion } from "framer-motion";
+import { useUserStore } from "@/lib/stores/UserStore";
+
+export function Receipt(props: {
+  question: string;
+  title: string;
+  multiplier: number;
+  points: number;
+  image: string;
+  option: number;
+  options: [];
+  id: number;
+  isDesktop?: boolean;
+}) {
+  const { user } = useUserStore();
+
+  const shareLink = async () => {
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: `${props.title} on Glimpse`,
+          text: "This prediction on Glimpse is wild! Check it out",
+          url: `https://pwa-8-ball.vercel.app/p/${props.id}`,
+        });
+        console.log("Data was shared successfully");
+      } catch (err) {
+        console.error("Share failed:", err);
+      }
+    } else {
+      console.log("Web Share not supported on this browser");
+    }
+  };
+
+  return (
+    <div
+      className={`flex m-3 mt-5 border-2 border-[#151515] flex-col items-center ${
+        props.isDesktop ? "bg-transparent" : "bg-[#080808]"
+      }  ${props.isDesktop ? "p-1" : "p-5"}  ${
+        props.isDesktop ? "rounded-[20px]" : "rounded-[20px]"
+      }`}
+    >
+      <div
+        className={`flex flex-col w-full ${
+          props.isDesktop ? "bg-transparent" : "bg-[#080808]"
+        } rounded-[20px]`}
+      >
+        <div className="flex flex-row items-center justify-between w-full">
+          <div className="w-[37px]"></div>
+          <div className="flex flex-row items-center p-[5px] rounded-[15px] bg-[#181818]">
+            <div className="p-[5px] rounded-[10px] bg-green-500">
+              <ArrowDown color={"white"} strokeWidth={3.5} size={18} />
+            </div>
+            <span className="text-[17px] text-white mx-[6px] font-semibold">
+              {props.options[props.option]}
+            </span>
+          </div>
+          <motion.div
+            onClick={() => (props.isDesktop ? props.changeStep(4) : onClose())}
+            className="py-[14.5px] px-[8.5px] rounded-[2px] cursor-pointer"
+          >
+            <X color={"#585858"} strokeWidth={5} height={18} />
+          </motion.div>
+        </div>
+        <div className="flex flex-row items-center justify-center">
+          <div className="text-[3em] font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#dcedc1] via-white to-[#a8e6cf] my-[10px]">
+            ${props.points}
+          </div>
+        </div>
+        <div className="border border-dashed border-[#212121] mb-[15px]"></div>
+        <div className="flex flex-row items-center justify-between my-[15px] w-[99%]">
+          <span className="text-[17.5px] text-[#D3D3D3]">Market</span>
+          <div className="flex flex-row items-center">
+            <span className="text-[20px] text-white mr-[9px]">
+              {props.title}
+            </span>
+            <img
+              className="h-[38px] object-cover w-[38px] rounded-[10px] overflow-hidden"
+              src={props?.image}
+              alt="Market"
+            />
+          </div>
+        </div>
+        <div className="flex flex-row items-center justify-between my-[15px] w-[99%]">
+          <span className="text-[17.5px] text-[#D3D3D3]">Predicted on</span>
+          <div className="flex flex-row items-center">
+            <span className="text-[19px] text-[#D3D3D3] mr-[4px]">2024</span>
+            <span className="text-[19px] text-white mr-[7px]">Thursday</span>
+            <div className="flex flex-col p-[7px] py-[1px] w-[41px] h-[42px] rounded-[10px] bg-[#181818] items-center justify-center">
+              <span className="text-[8px] text-[#FF0050] mb-[-10px]">Sept</span>
+              <span className="text-[25px] text-white">21</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-row items-center justify-between my-[15px] w-[99%]">
+          <span className="text-[17.5px] text-[#D3D3D3]">Predictoor</span>
+          <div className="flex flex-row items-center">
+            <span className="text-[20px] mr-[8px] text-white">{user.name}</span>
+            <img
+              className="h-[38px] w-[38px] rounded-[10px] overflow-hidden"
+              src={user.pfp}
+              alt="Pen"
+            />
+          </div>
+        </div>
+        <div className="flex flex-row items-center justify-between my-[15px] w-[99%]">
+          <span className="text-[17.5px] text-[#D3D3D3]">Potential Payout</span>
+          <span className="text-[20px] text-white ">
+            ${(props.points * 3).toFixed(2)}
+          </span>
+        </div>
+      </div>
+      <div className="flex flex-row items-center gap-[5px] my-[5px]">
+        <div
+          onClick={shareLink}
+          className={`mt-[12px] flex flex-row p-[8px] rounded-[24px] overflow-hidden bg-[#D9D9D9] ${
+            props?.isDesktop ? "w-[24vw]" : `w-[85vw]`
+          } items-center justify-center cursor-pointer`}
+        >
+          <div className="flex flex-row items-center justify-center">
+            <ShareIcon color="black" strokeWidth={3} height={23} />
+            <span className="text-[20px] text-[#1D1D1D] font-extrabold ml-[3px]">
+              Share
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/up/[id].tsx
+++ src/pages/up/[id].tsx
@@ -1,17 +1,25 @@
 // @ts-nocheck
 
+import { Receipt } from "@/components/share/UserPosition.tsx";
 
-
-export default function MarketPage({ params, searchParams }: {
+export default function MarketPage({
+  params,
+  searchParams,
+}: {
   params: { id: string };
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const { id } = params ?? {};
   // const metadata = generateMetadata({ params, searchParams });
   return (
-    <>
-
-      <div>{/* Your page component logic */}</div>
-    </>
+    <Receipt
+      title="Base 10mil Mau"
+      points={3920}
+      image="https://images.mirror-media.xyz/publication-images/yyhMh-YKnMgC0jETSQqvv.png?height=1200&width=2400"
+      options={["Yes", "No"]}
+      option={1}
+      id={11}
+      question="Will Base reach 10 million monthly active wallets during onchain summer?"
+    />
   );
 }
