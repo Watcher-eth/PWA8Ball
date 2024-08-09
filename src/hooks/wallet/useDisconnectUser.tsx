2a99857fe9ffb0f935cdcef19d0ec29e7329608f
watcher-eth1 <watchereth10@gmail.com>
2024-08-09 20:41:27 +0200

    updated toast logout

--- src/hooks/wallet/useDisconnectUser.tsx
+++ src/hooks/wallet/useDisconnectUser.tsx
@@ -1,5 +1,6 @@
 import { useUserStore } from "@/lib/stores/UserStore";
 import { usePrivy } from "@privy-io/react-auth";
+import { DoorOpen } from "lucide-react";
 import { toast } from "sonner";
 import { useDisconnect } from "wagmi";
 
@@ -13,7 +14,24 @@ export function useDisconnectUser() {
       try {
         disconnect();
         setUser(null);
-        toast.success("Logged out successfully!");
+        toast(
+          <div className="w-full rounded-full bg-[#101010] font-[600] text-[1rem] px-3 pr-4 text-white flex flex-row items-center p-2">
+            <div className="p-0.5 py-1.5 rounded-full bg-[#323232] mr-2 flex justify-center items-center">
+              <DoorOpen strokeWidth={3} className="text-white h-[1rem]" />
+            </div>
+            Logged out succesfully!
+          </div>,
+          {
+            unstyled: true,
+            classNames: {
+              title: "text-red-400 text-2xl",
+              description: "text-red-400",
+              actionButton: "bg-zinc-400",
+              cancelButton: "bg-orange-400",
+              closeButton: "bg-lime-400",
+            },
+          }
+        );
       } catch (e) {
         toast.warning(
           "Error logging out! Please reload the page and try again."
@@ -23,7 +41,24 @@ export function useDisconnectUser() {
       try {
         logout();
         setUser(null);
-        toast.success("Logged out successfully!");
+        toast(
+          <div className="w-full rounded-full bg-[#101010] font-[600] text-[1rem] px-3 pr-4 text-white flex flex-row items-center p-2">
+            <div className="p-0.5 py-1.5 rounded-full bg-[#323232] mr-2 flex justify-center items-center">
+              <DoorOpen strokeWidth={3} className="text-white h-[1rem]" />
+            </div>
+            Logged out succesfully!
+          </div>,
+          {
+            unstyled: true,
+            classNames: {
+              title: "text-red-400 text-2xl",
+              description: "text-red-400",
+              actionButton: "bg-zinc-400",
+              cancelButton: "bg-orange-400",
+              closeButton: "bg-lime-400",
+            },
+          }
+        );
       } catch (e) {
         toast.warning(
           "Error logging out! Please reload the page and try again."
@@ -34,4 +69,3 @@ export function useDisconnectUser() {
 
   return { disconnectUser };
 }
-
