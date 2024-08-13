f3f36a288d7f609991e1137fc14549c57c421828
caesar <caesar0x@users.noreply.github.com>
2024-08-13 00:29:16 -0400

    fucking shocker

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { init, AirstackProvider } from "@airstack/airstack-react";
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { PrivyProvider } from "@privy-io/react-auth";
-import * as amplitude from "@amplitude/analytics-browser";
+// import * as amplitude from "@amplitude/analytics-browser";
 
 import { SonnerToaster } from "@/components/ui/SonnerToaster";
 
@@ -47,9 +47,9 @@ export const wagmiConfig = getDefaultConfig({
 export default function App({ Component, pageProps, router }: AppProps) {
   // console.log(router)
   // console.log({pageProps})
-  amplitude.init("3b52857bdc943ed9b3ec5ac60e5dbba1", {
-    autocapture: { elementInteractions: true },
-  });
+  // amplitude.init("3b52857bdc943ed9b3ec5ac60e5dbba1", {
+  //   autocapture: { elementInteractions: true },
+  // });
   useServiceWorker(); // Use the custom hook
 
   return (
