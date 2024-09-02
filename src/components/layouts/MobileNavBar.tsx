b597edac4cd14f94fb8b90eb556b70054d05a672
watcher-eth1 <watchereth10@gmail.com>
2024-09-02 15:03:43 +0200

    Mobile looking fuckn clean now

--- src/components/layouts/MobileNavBar.tsx
+++ src/components/layouts/MobileNavBar.tsx
@@ -24,39 +24,42 @@ export function MobileNavBar() {
   const { isLoginModalOpen, openLoginModal, closeLoginModal } =
     useAuthModalStore();
   return (
-    <div
-      className={`
-      bg-[#212121]/20 items-center mb-1 justify-center p-[0.9rem] rounded-full 
-      z-3 self-center border-[0.5px] border-[rgba(90,90,90,0.2)] backdrop-blur-lg
+    <div className="flex flex-col items-center -mb-6 justify-center w-full ">
+      <div
+        className={`
+      bg-[#212121]/20  p-[0.9rem] rounded-full 
+      z-3 self-center border-[0.5px] border-[rgba(90,90,90,0.2)] -mb-8 backdrop-blur-lg
     `}
-    >
-      <div className="w-[60vw] max-w-[17rem] flex px-2  justify-between items-center mx-auto">
-        <MobileNavBtn IconComponent={Home} href={HOME_PATH} />
-        <MobileNavBtn IconComponent={Search} href={SEARCH_PATH} />
+      >
+        <div className="w-[60vw] max-w-[17rem] flex px-2  justify-between items-center mx-auto">
+          <MobileNavBtn IconComponent={Home} href={HOME_PATH} />
+          <MobileNavBtn IconComponent={Search} href={SEARCH_PATH} />
 
-        {user?.walletAddress ? (
-          <>
-            <MobileNavBtn IconComponent={ActivityIcon} href={ACTIVITY_PATH} />
-            <MobileNavBtn
-              iconSrc={user?.pfp}
-              IconComponent={UserCircle}
-              href={getProfilePath(user?.walletAddress)}
-            />
-          </>
-        ) : (
-          <>
-            <MobileNavBtn
-              IconComponent={ActivityIcon}
-              onClick={openLoginModal}
-            />
-            <MobileNavBtn
-              iconSrc={user?.pfp}
-              IconComponent={UserCircle}
-              onClick={openLoginModal}
-            />
-          </>
-        )}
+          {user?.walletAddress ? (
+            <>
+              <MobileNavBtn IconComponent={ActivityIcon} href={ACTIVITY_PATH} />
+              <MobileNavBtn
+                iconSrc={user?.pfp}
+                IconComponent={UserCircle}
+                href={getProfilePath(user?.walletAddress)}
+              />
+            </>
+          ) : (
+            <>
+              <MobileNavBtn
+                IconComponent={ActivityIcon}
+                onClick={openLoginModal}
+              />
+              <MobileNavBtn
+                iconSrc={user?.pfp}
+                IconComponent={UserCircle}
+                onClick={openLoginModal}
+              />
+            </>
+          )}
+        </div>
       </div>
+      <div className="flex flex-col items-center bg-gradient-to-t from-[#151515]/95 via-[#151515]/70  to-[transparent]  justify-center w-full h-14 "></div>
     </div>
   );
 }
