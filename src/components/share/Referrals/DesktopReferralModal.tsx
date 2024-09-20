6a865f02a5eb62b0550027a6eed1c1ef34b3ad1a
watcher-eth1 <watchereth10@gmail.com>
2024-09-20 16:55:11 +0200

    referral modal pt2

--- src/components/share/Referrals/DesktopReferralModal.tsx
+++ src/components/share/Referrals/DesktopReferralModal.tsx
@@ -3,7 +3,7 @@ import { ProfileCard } from "./ProfileCard"
 import { useUserStore } from "@/lib/stores/UserStore"
 import { useUsdcBalance } from "@/hooks/wallet/useUsdcBalance"
 import { DesktopCardModal } from "@/components/modals/DesktopCardModal"
-
+import { motion } from "framer-motion"
 function DesktopReferralModalContent() {
   const { user } = useUserStore()
   const balance = useUsdcBalance({ address: user?.walletAddress })
@@ -21,13 +21,63 @@ export function DesktopReferralModal({
 }) {
   return (
     <DesktopCardModal
-      dialogContentClassName="rounded-[1.5rem] "
-      dialogClassName="xl:max-w-[28vw] md:max-w-[38vw] sm:max-w-[60vw]"
-      cardClassName="xl:max-w-[28vw] md:max-w-[38vw] sm:max-w-[60vw] "
+      dialogContentClassName="rounded-[1.5rem] xl:max-w-[32vw]  md:max-w-[41vw] sm:max-w-[60vw] "
+      dialogClassName="xl:max-w-[32vw] md:max-w-[41vw] sm:max-w-[60vw]"
+      cardClassName="xl:max-w-[32vw] lg:max-w-[45vw] md:max-w-[41vw] sm:max-w-[60vw] "
       cardContentClassName="bg-[#080808]/75  rounded-[1.5rem] p-0 h-[75vh] backdrop-blur-lg"
       content={<DesktopReferralModalContent />}
     >
       {children}
     </DesktopCardModal>
+  )
+}
+
+function ReferallItem(props: {
+  image: string
+  title: string
+  name: string
+  pfp: string
+  amount: number
+  date: string
+  index: number
+}) {
+  return (
+    <div className="flex flex-row items-center justify-between">
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: index * 0.15 }}
+        className={`flex flex-row items-center justify-between`}
+      >
+        <div className="flex flex-row items-center relative">
+          <img
+            src={props.image}
+            alt="Prediction"
+            className={`h-[50px] w-[50px] object-cover ${"rounded-[10px]"}`}
+          />
+          <img
+            src={props.pfp}
+            alt="Profile"
+            className="h-[25px] w-[25px] object-cover rounded-[15px] absolute bottom-[-6px] left-[32px] border-[3px] border-[#1B1B1E]"
+          />
+          <div className="flex flex-col -space-y-0.5 ml-[12.5px] mr-[-36px] max-w-full">
+            <h3 className="text-[18px] text-white font-semibold">
+              {props.name}
+            </h3>
+            <p className="text-[15.5px] text-[lightgray] font-normal overflow-hidden whitespace-nowrap text-ellipsis">
+              {props.title}
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col items-end space-y-1">
+          <p
+            className={`text-[14px] px-2 py-0 rounded-md text-white font-semibold`}
+          >
+            {props.date}
+          </p>
+          <p className="text-[16px] text-white font-[600]">{props.amount}</p>
+        </div>
+      </motion.div>
+    </div>
   )
 }
--- src/components/share/Referrals/ProfileCard.tsx
+++ src/components/share/Referrals/ProfileCard.tsx
@@ -6,42 +6,28 @@ export const ProfileCard = ({ user, userBalance, onClose }) => {
   const [toastVisible, setToastVisible] = useState(false)
 
   return (
-    <div className="flex  items-center bg-[#090909] shadow-md border-[0.1rem] border-[#121212] p-5  rounded-lg">
-      <div className="flex  justify-between w-full h-[19.5vh]">
+    <div className="flex  items-center bg-[#090909] shadow-md border-[0.1rem] border-[#121212] p-5 mt-6 rounded-lg">
+      <div className="flex  justify-between w-full ">
         <div className="flex flex-col justify-between">
-          <img
-            src={user.pfp}
-            alt={`${user.name}'s profile`}
-            className="h-12 w-12 rounded-full"
-          />
           <div className="flex flex-col">
-            <span className="text-white text-3xl font-[600]">
-              ${(Number(userBalance) / 1000000).toFixed(2)}
-            </span>
-            <span className="text-[lightgray] font-[400]">
+            <span className="text-[lightgray] text-sm font-[400]">
               Referrals earned
+            </span>
+            <span className="text-white text-3xl mt-1 font-[600]">
+              ${(Number(userBalance) / 1000000).toFixed(2)}
             </span>
           </div>
         </div>
-        <div className="flex flex-col justify-between items-end">
+        <div className="flex flex-col  items-end">
           <button
-            onClick={() =>
+            onClick={() => {
               copyToClipboard(
                 `https://pwa-8-ball.vercel.app/?ref=${user?.walletAddress}`
               )
-            }
-            className="flex hover:scale-101 active:scale-99 items-center text-white"
-          >
-            <span className="mr-2">Copy Address</span>
-            <Copy className="h-4 w-4 text-[lightgray]" />
-          </button>
-          <button
-            onClick={() => {
-              /* Navigate to Add Funds */
             }}
             className="mt-2 bg-[#151515] hover:scale-101 active:scale-99 border-[0.1rem] border-[#191919] text-white rounded-full px-4 py-1.5"
           >
-            Share your Link
+            Copy your Link
           </button>
         </div>
       </div>
