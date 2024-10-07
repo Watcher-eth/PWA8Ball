c2ad084739eeed03b0455e2224e78ed75ac69018
Watcher <watchereth10@gmail.com>
2024-10-06 19:27:18 -0700

    added root opertor

--- src/constants/onchain.ts
+++ src/constants/onchain.ts
@@ -4,6 +4,6 @@ import { base, baseSepolia } from "viem/chains"
 
 export const RootOperatorAddress = {
   [baseSepolia.id]: "0xF1068eCe146Be3E4aC1d90f739C5a42A8bFc5853",
-  [base.id]: "",
+  [base.id]: "0x3DE76DBB97e0EfD0ea8287082dF450cc59927574",
 } as const
 
